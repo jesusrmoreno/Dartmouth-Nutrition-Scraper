f65308fc814f1f53be0280dc113593325224a24b
Jesus Moreno <jesusrmor94@gmail.com>
2015-09-28 11:16:13 -0400

    Add: Comments

--- main.go
+++ main.go
@@ -274,36 +274,53 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	concurrency := 15
-	startTime := time.Now()
 
+	// We can open up to 50 connections but only about 15 of them can be handled
+	// at any one time so we cap the concurrency at 15
+	concurrency := 15
+
+	startTime := time.Now()
 	fmt.Println("Starting scrape at: ", startTime)
 	defer timeTrack(startTime, "Entire Scrape")
 
+	// sem will keep track of the number of concurrent connections and server as
+	// a semaphore
 	sem := make(chan bool, concurrency)
-	venues := make(chan bool)
+
+	// venues is not used right now but will be used to work with venues as they
+	// are filled in with their information.
+	venues := make(chan constants.VenueInfo)
+
+	// Make sure that our channels get closed
 	defer func() { close(sem) }()
 	defer func() { close(venues) }()
 
 	for key, venue := range sids {
+
 		fmt.Println("Starting", key, "at:", time.Now())
+
 		info := constants.VenueInfo{}
 		sid, err := GetSID(key)
 		if err != nil {
 			panic(err)
 		}
+
 		info.Venue = venue
 		info.Key = key
 		info.SID = sid
+
 		info.Menus, err = GetMenuList(sid)
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		info.Meals, err = GetMealList(sid)
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		fmt.Println("Getting recipes for ", key, "at", time.Now())
+
 		for _, menu := range info.Menus {
 			for _, meal := range info.Meals {
 				newRecipes, err := GetRecipesMenuMealDate(sid, menu.ID, meal.ID)
@@ -314,24 +331,39 @@ func main() {
 				info.Recipes = append(info.Recipes, newRecipes...)
 			}
 		}
+
+		// This section is the part that benefits the most from concurrency
+		// the top parts finish in about 5 seconds but this will take up to
+		// 15 minutes if done one by one.
 		bar := pb.StartNew(len(info.Recipes))
 		bar.ShowSpeed = true
 		bar.SetMaxWidth(80)
 		for index := range info.Recipes {
+
+			// Start a new goroutine for each nutrition request
 			go func(key string, index int, info *constants.VenueInfo) {
+
+				// Read from the semaphore after we are done to free up a space for the
+				// next connection
 				defer func() {
 					<-sem
 					bar.Increment()
 				}()
+
 				_, err := GetNutrients(info.SID, &info.Recipes[index])
+
 				if err != nil {
 					log.Println(err.(*errors.Error).ErrorStack())
 				}
+
 			}(key, index, &info)
+
 			sem <- true
+
 		}
 		bar.FinishPrint("Done getting recipes for: " + key)
 	}
+
 	for i := 0; i < cap(sem); i++ {
 		sem <- true
 	}
